4e070f5e0dc0be3cf2365963f226075058f316c2
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-11-03 12:35:30 -0500

    initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function App() {
+
+	// redux dispatch
+	const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>eCommerce App</h1>
+			
+			
     </div>
   );
 }
